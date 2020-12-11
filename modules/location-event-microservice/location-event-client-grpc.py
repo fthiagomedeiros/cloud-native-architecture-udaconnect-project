fc7d7b6d6da83422a7c09363c19b52f2ae99e2e9
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2020-12-10 21:27:09 -0300

    location-processor-microservice

--- modules/location-event-microservice/location-event-client-grpc.py
+++ modules/location-event-microservice/location-event-client-grpc.py
@@ -12,7 +12,7 @@ print("Coordinates sent...")
 channel = grpc.insecure_channel("localhost:5005")
 stub = coordinates_event_pb2_grpc.ItemServiceStub(channel)
 
-for i in range(100000):
+for i in range(10):
     # Update this with desired payload
     user_coordinates = coordinates_event_pb2.EventCoordinatesMessage(
         userId=i,
