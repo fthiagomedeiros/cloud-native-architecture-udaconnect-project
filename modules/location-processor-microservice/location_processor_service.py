d5d89ff58e8caae48dab8bd520d223691a8ae748
Francisco Thiago de Macedo Medeiros <fthiagomedeiros@gmail.com>
2020-12-19 21:45:19 -0300

    implementing processor for location microservice

--- modules/location-processor-microservice/location_processor_service.py
+++ modules/location-processor-microservice/location_processor_service.py
@@ -1,25 +1,39 @@
 import json
 import os
-import logging
 
 from kafka import KafkaConsumer
 
+TOPIC_NAME = 'test'
+print('started listening ' + TOPIC_NAME)
+
+consumer = KafkaConsumer(TOPIC_NAME)
+
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
 KAFKA_URL = os.environ["KAFKA_URL"]
-KAFKA_TOPIC = os.environ["KAFKA_TOPIC"]
-
-print('KAFKA URL ' + KAFKA_URL)
-print('started listening topic ' + KAFKA_TOPIC)
-
-consumer = KafkaConsumer(KAFKA_TOPIC, bootstrap_servers=KAFKA_URL)
 
 
-def save_in_db(_location):
-    print('processing print location ', _location)
-    logging.info('processing log location ', _location)
+def save_in_db(location):
+    from sqlalchemy import create_engine
+
+    engine = create_engine(f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
+    conn = engine.connect()
+
+    person_id = int(location["userId"])
+    latitude, longitude = int(location["latitude"]), int(location["longitude"])
+
+    insert = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}))" \
+        .format(person_id, latitude, longitude)
+
+    print(insert)
+    conn.execute(insert)
 
 
 for location in consumer:
     message = location.value.decode('utf-8')
-    # print('{}'.format(message))
+    print('{}'.format(message))
     location_message = json.loads(message)
     save_in_db(location_message)
